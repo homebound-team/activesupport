b73331f498cbbbc6dd5b474901b624d6b6573161
Ben Limmer <630449+blimmer@users.noreply.github.com>
2023-05-19 07:30:49 -0600

    feat(array): add uniqueByKey (#2)

--- src/array.test.ts
+++ src/array.test.ts
@@ -82,4 +82,18 @@ describe("array", () => {
       expect(arr.indexOf(2)).toBe(-1);
     });
   });
+
+  describe("uniqueByKey", () => {
+    it("removes duplicates", () => {
+      const arr = [{ foo: 1 }, { foo: 2 }, { foo: 1 }];
+      const result = arr.uniqueByKey("foo");
+      expect(result).toEqual([{ foo: 1 }, { foo: 2 }]);
+    });
+
+    it("uses the first object if there are multiple matching elements in the array", () => {
+      const arr = [{ foo: 1, bar: 2 }, { foo: 2 }, { foo: 1, baz: 3 }];
+      const result = arr.uniqueByKey("foo");
+      expect(result).toEqual([{ foo: 1, bar: 2 }, { foo: 2 }]);
+    });
+  });
 });
