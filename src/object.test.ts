16cf159c15c090bbaa0745d7973b668d08d23d63
Ben Limmer <ben@benlimmer.com>
2023-06-02 12:17:25 -0600

    better test

--- src/object.test.ts
+++ src/object.test.ts
@@ -1,17 +1,28 @@
+import { readFileSync } from "fs";
+import { join } from "path";
 import "./object";
 
 describe("object", () => {
-  it("allows classes to overwrite activesupport property extensions", () => {
-    const methods = ["toEntries", "toValues", "toKeys"];
-    methods.forEach((method) => {
-      class Test {
-        constructor() {
-          this[method] = () => "overwritten";
-        }
-      }
+  const methods = extractObjectExtensions();
 
-      const test = new Test();
-      expect(test[method]()).toEqual("overwritten");
-    });
+  it.each(methods)("allows classes to overwrite activesupport property extension - %s", (method) => {
+    class Test {
+      constructor() {
+        this[method] = () => "overwritten";
+      }
+    }
+
+    const test = new Test();
+    expect(test[method]()).toEqual("overwritten");
   });
 });
+
+function extractObjectExtensions(): string[] {
+  // This is pretty basic right now - if the file becomes more complex, consider using an AST parser.
+  const file = readFileSync(join(__dirname, "object.ts"), "utf8");
+  const methods = [...file.matchAll(/Object\.defineProperty\(Object\.prototype, "(\w+)"/g)].map((match) => match[1]);
+
+  if (methods.length === 0) throw new Error("Could not extract extension methods using regex!");
+
+  return methods;
+}
