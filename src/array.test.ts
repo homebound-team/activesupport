a39f4189bffa0aee6e8c7bc4ea1d7b84a8d836db
Chris Fox <fox@outlook.com>
2023-12-01 13:29:05 -0500

    feat: Support for bigint on sort/sortBy/sortByKey (#9)
    New PR to force a semantic release.

--- src/array.test.ts
+++ src/array.test.ts
@@ -18,6 +18,12 @@ describe("array", () => {
     it("sorts using specified the specified function", () => {
       expect([1, 2, 3].sortBy((n: number) => -n)).toEqual([3, 2, 1]);
     });
+
+    it("works for bigint fields", () => {
+      // Using `any` here as an easy case for the compiler not being able to catch the error
+      const foos: { foo: bigint }[] = [{ foo: 3n }, { foo: -1n }, { foo: 2n }];
+      expect(foos.sortBy((f) => f.foo)).toEqual([{ foo: -1n }, { foo: 2n }, { foo: 3n }]);
+    });
   });
 
   describe("sortByKey", () => {
