07f1f3f104595f1e1e59b803c6f32da2877129c6
Arvin Siva <asiva@homebound.com>
2024-01-30 13:19:44 -0500

    feat: add asyncSome to array

--- src/array.test.ts
+++ src/array.test.ts
@@ -190,4 +190,42 @@ describe("array", () => {
       ).toThrow();
     });
   });
+
+  describe("asyncSome", () => {
+    it("returns true if any element matches async predicate (even number)", async () => {
+      // given an array of even and odd numbers
+      const a = [1, 2, 3, 4, 5];
+      // when we call asyncSome with an async predicate that checks for even numbers
+      const result = await a.asyncSome((el) => Promise.resolve(el % 2 === 0));
+      // then expect asyncSome to return true
+      expect(result).toBe(true);
+    });
+
+    it("returns true if any element matches sync/async predicate (1 or even number)", async () => {
+      // given an array of even and odd numbers which includes 1
+      const a = [1, 2, 3, 4, 5];
+      // when we call asyncSome with a sync/async predicate that checks for 1 or even numbers
+      const result = await a.asyncSome((el) => el === 1 || Promise.resolve(el % 2 === 0));
+      // then expect asyncSome to return true
+      expect(result).toBe(true);
+    });
+
+    it("returns false if no elements match sync/async predicate (7 or even number)", async () => {
+      // given an array of odd numbers
+      const a = [1, 3, 5];
+      // when we call asyncSome with a sync/async predicate that checks for 7 or even numbers
+      const result = await a.asyncSome((el) => el === 7 || Promise.resolve(el % 2 === 0));
+      // then expect asyncSome to return false
+      expect(result).toBe(false);
+    });
+
+    it("returns false if the array is empty", async () => {
+      // given an empty array
+      const a: number[] = [];
+      // when we call asyncSome with an async predicate that checks for even numbers
+      const result = await a.asyncSome((el) => Promise.resolve(el % 2 === 0));
+      // then expect asyncSome to return false
+      expect(result).toBe(false);
+    });
+  });
 });
