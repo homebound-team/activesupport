8f0f2904a697087dbecbdb8fb4cc5548b5ddb2d6
zgavin <234354+zgavin@users.noreply.github.com>
2024-03-14 20:52:07 -0600

    feat: Added uniqueBy and asyncFlatMap (#11)

--- src/array.test.ts
+++ src/array.test.ts
@@ -109,6 +109,20 @@ describe("array", () => {
     });
   });
 
+  describe("uniqueBy", () => {
+    it("removes duplicates", () => {
+      const arr = [{ foo: 1 }, { foo: 2 }, { foo: 1 }];
+      const result = arr.uniqueBy((obj) => obj.foo);
+      expect(result).toEqual([{ foo: 1 }, { foo: 2 }]);
+    });
+
+    it("uses the first object if there are multiple matching elements in the array", () => {
+      const arr = [{ foo: 1, bar: 2 }, { foo: 2 }, { foo: 1, baz: 3 }];
+      const result = arr.uniqueBy((obj) => obj.foo);
+      expect(result).toEqual([{ foo: 1, bar: 2 }, { foo: 2 }]);
+    });
+  });
+
   describe("everyHasSame", () => {
     it("returns true if all elements match", () => {
       // given everything has the same 'name' property
