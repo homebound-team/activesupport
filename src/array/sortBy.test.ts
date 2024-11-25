9df23570ba66bde9d231fe983ffde20a29966a48
Stephen Haberman <shaberman@homebound.com>
2024-11-25 08:55:38 -0600

    feat: Support sorting by multiple values.

--- src/array/sortBy.test.ts
+++ src/array/sortBy.test.ts
@@ -5,6 +5,21 @@ describe("sortBy", () => {
     expect([1, 2, 3].sortBy((n: number) => -n)).toEqual([3, 2, 1]);
   });
 
+  it("sorts by two values", () => {
+    const foos: { foo: number; bar: number }[] = [
+      { foo: 3, bar: 2 },
+      { foo: 1, bar: 0 },
+      { foo: 3, bar: 1 },
+      { foo: 2, bar: 0 },
+    ];
+    expect(foos.sortBy((f) => [f.foo, f.bar])).toEqual([
+      { foo: 1, bar: 0 },
+      { foo: 2, bar: 0 },
+      { foo: 3, bar: 1 },
+      { foo: 3, bar: 2 },
+    ]);
+  });
+
   it("works for bigint fields", () => {
     // Using `any` here as an easy case for the compiler not being able to catch the error
     const foos: { foo: bigint }[] = [{ foo: 3n }, { foo: -1n }, { foo: 2n }];
