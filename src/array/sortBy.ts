9888cce45c951f783afcd6687f692082d035a137
Stephen Haberman <shaberman@homebound.com>
2024-11-25 20:47:37 -0600

    Runtime enforce array sameness.

--- src/array/sortBy.ts
+++ src/array/sortBy.ts
@@ -21,6 +21,9 @@ Array.prototype.sortBy = function <T, K extends Comparable>(this: T[], fn: (el: 
     const av = fn(a);
     const bv = fn(b);
     if (Array.isArray(av) && Array.isArray(bv)) {
+      if (av.length !== bv.length) {
+        throw new Error("sortBy cannot compare arrays of different lengths");
+      }
       for (let i = 0; i < av.length; i++) {
         const comparison = compare(av[i], bv[i]);
         if (comparison !== 0) return comparison;
