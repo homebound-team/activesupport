202af6f7a40356e995aba9ada358b7294eebec32
Stephen Haberman <shaberman@homebound.com>
2025-05-08 15:04:52 -0500

    fix: Fix mapToObject with a const return type. (#23)

--- src/array/mapToObject.ts
+++ src/array/mapToObject.ts
@@ -3,21 +3,21 @@ import { CallbackFn, CallbackFnRO } from "./index";
 
 declare global {
   interface Array<T> {
-    mapToObject<K extends PropertyKey, V>(fn: CallbackFn<T, [K, V]>): Record<K, V>;
-    mapToObject<K extends PropertyKey, V>(fn: CallbackFn<T, Promise<[K, V]>>): Promise<Record<K, V>>;
+    mapToObject<K extends PropertyKey, V>(fn: CallbackFn<T, readonly [K, V]>): Record<K, V>;
+    mapToObject<K extends PropertyKey, V>(fn: CallbackFn<T, Promise<readonly [K, V]>>): Promise<Record<K, V>>;
   }
 
   interface ReadonlyArray<T> {
-    mapToObject<K extends PropertyKey, V>(fn: CallbackFnRO<T, [K, V]>): Record<K, V>;
-    mapToObject<K extends PropertyKey, V>(fn: CallbackFnRO<T, Promise<[K, V]>>): Promise<Record<K, V>>;
+    mapToObject<K extends PropertyKey, V>(fn: CallbackFnRO<T, readonly [K, V]>): Record<K, V>;
+    mapToObject<K extends PropertyKey, V>(fn: CallbackFnRO<T, Promise<readonly [K, V]>>): Promise<Record<K, V>>;
   }
 }
 
-function mapToObject<T, K extends PropertyKey, V>(fn: CallbackFn<T, [K, V]>): Record<K, V>;
-function mapToObject<T, K extends PropertyKey, V>(fn: CallbackFn<T, Promise<[K, V]>>): Promise<Record<K, V>>;
+function mapToObject<T, K extends PropertyKey, V>(fn: CallbackFn<T, readonly [K, V]>): Record<K, V>;
+function mapToObject<T, K extends PropertyKey, V>(fn: CallbackFn<T, Promise<readonly [K, V]>>): Promise<Record<K, V>>;
 function mapToObject<T, K extends PropertyKey, V>(
   this: T[],
-  fn: CallbackFn<T, MaybePromise<[K, V]>>,
+  fn: CallbackFn<T, MaybePromise<readonly [K, V]>>,
 ): MaybePromise<Record<K, V>> {
   const maybePromises = this.map(fn);
   return maybePromiseAllThen(maybePromises, (r) => r.toObject());
