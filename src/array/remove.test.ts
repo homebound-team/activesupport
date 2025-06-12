4325d8c4c8e430e116bee01f1d53fca5fc24c717
Zachary Gavin <zgavin@homebound.com>
2025-06-12 10:09:39 -0600

    feat: Allow passing multiple arguments to Array.remove

--- src/array/remove.test.ts
+++ src/array/remove.test.ts
@@ -9,4 +9,13 @@ describe("remove", () => {
     // Then the array is mutated and the element removed
     expect(a).toEqual(["a", "c"]);
   });
+
+  it("removes multiiple elements from an array", () => {
+    // Given an array of strings
+    const a = ["a", "b", "c", "d", "e"];
+    // When we remove an element
+    a.remove("b", "d");
+    // Then the array is mutated and the element removed
+    expect(a).toEqual(["a", "c", "e"]);
+  });
 });
--- src/array/remove.ts
+++ src/array/remove.ts
@@ -3,12 +3,15 @@ export {}; // needed for TS to realize this file can be imported
 declare global {
   interface Array<T> {
     remove(element: T): void;
+    remove(...elements: T[]): void;
   }
 }
 
-Array.prototype.remove = function (element) {
-  const index = this.indexOf(element);
-  if (index > -1) {
-    this.splice(index, 1);
+Array.prototype.remove = function <T>(this: T[], ...elements: T[]) {
+  // start from the end of the array so we don't need to worry about re-ordering
+  for (let index = this.length - 1; index >= 0; index--) {
+    if (elements.includes(this[index])) {
+      this.splice(index, 1);
+    }
   }
 };
