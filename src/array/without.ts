0e88e29d4b80791daa56db110e8df3de0663ee5e
zgavin <234354+zgavin@users.noreply.github.com>
2025-06-12 08:59:04 -0700

    feat: Allow passing multiple arguments to Array.without (#30)

--- src/array/without.ts
+++ src/array/without.ts
@@ -3,15 +3,19 @@ export {}; // needed for TS to realize this file can be imported
 declare global {
   interface Array<T> {
     without(element: T): Array<T>;
+    without(...elements: T[]): Array<T>;
   }
 
   interface ReadonlyArray<T> {
     without(element: T): Array<T>;
+    without(...elements: T[]): Array<T>;
   }
 }
 
-Array.prototype.without = function (element) {
+Array.prototype.without = function <T>(this: T[], ...elements: T[]): Array<T> {
   const result = [...this];
-  result.remove(element);
+  for (const element of elements) {
+    result.remove(element);
+  }
   return result;
 };
