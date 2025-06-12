1f1ccd361e05d9f086ad2bfafa9dd03ac9dd5441
Zachary Gavin <zgavin@homebound.com>
2025-06-12 09:44:53 -0600

    feat: Allow passing multiple arguments to Array.without

--- src/array/without.test.ts
+++ src/array/without.test.ts
@@ -11,4 +11,15 @@ describe("without", () => {
     // And the new array is changed
     expect(b).toEqual(["a", "c"]);
   });
+
+  it("removes multiple elements without side-effect", () => {
+    // Given an array of strings
+    const a = ["a", "b", "c", "d", "e"];
+    // When we remove an element
+    const b = a.without("b", "d");
+    // Then the original array is unchanged
+    expect(a).toEqual(["a", "b", "c", "d", "e"]);
+    // And the new array is changed
+    expect(b).toEqual(["a", "c", "e"]);
+  });
 });
