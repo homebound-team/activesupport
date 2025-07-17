7e2c8f59287a2abcc79c1d4dd2ee48c5da17819c
Stephen Haberman <shaberman@homebound.com>
2025-07-16 21:11:00 -0500

    feat: Add remove/without(Array) overloads.
    Also removes the `...` spread that can lead to max stack size errors:
    
    https://github.com/joist-orm/joist-orm/pull/1515/files

--- src/array/remove.ts
+++ src/array/remove.ts
@@ -2,13 +2,30 @@ export {}; // needed for TS to realize this file can be imported
 
 declare global {
   interface Array<T> {
+    /**
+     * Removes `element` by mutating the array.
+     *
+     * See `without` for a non-mutating version.
+     */
     remove(element: T): void;
+    /**
+     * Removes `elements` by mutating the array.
+     *
+     * See `without` for a non-mutating version.
+     */
+    remove(elements: readonly T[]): void;
+    /**
+     * Removes `elements` by mutating the array.
+     *
+     * See `without` for a non-mutating version.
+     */
     remove(...elements: readonly T[]): void;
   }
 }
 
-Array.prototype.remove = function <T>(this: T[], ...elements: readonly T[]) {
-  // start from the end of the array so we don't need to worry about re-ordering
+Array.prototype.remove = function <T>(this: T[], arg: T | T[]) {
+  // eslint-disable-next-line prefer-rest-params
+  const elements = Array.isArray(arg) ? arg : Array.from(arguments);
   for (let index = this.length - 1; index >= 0; index--) {
     if (elements.includes(this[index])) {
       this.splice(index, 1);
